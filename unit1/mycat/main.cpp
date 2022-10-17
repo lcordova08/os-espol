260e932884a2bde2d61ff7ecb45713a250ee87be
Lea Cordova <66025475+lcordova08@users.noreply.github.com>
2022-10-16 23:53:02 -0500

    Tarea

--- unit1/mycat/main.cpp
+++ unit1/mycat/main.cpp
@@ -1,26 +1,44 @@
-#include <iostream>
-#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 using namespace std;
+
+#define MAX_LEN 1024
 
 int main(int argc, char* argv[])
 {
-    string line;
-    
-    if (argc > 2) {
-        cout << "Use: mycat namefile." << endl;
-        return 0;
+    char input_line[MAX_LEN] = {0};  
+    if (argc == 2) {
+        int input_file = open(argv[1], O_RDONLY);
+        if(input_file < 0){
+            fprintf(stderr, "no se puede abrir el archivo %s\n", argv[1]);
+            exit(EXIT_FAILURE);
+        }
+        while(read(input_file, input_line, MAX_LEN) > 0 ){
+            size_t es = write(fileno(stdout), input_line, strlen(input_line) + 1);
+            if(es <= 0){
+                printf(stderr, "no se puede escribir en la salida\n");
+                exit(EXIT_FAILURE);
+            }
+            memset(input_line,0,MAX_LEN);
+        }
+        close(input_file);
     }
-
-    if (argc == 1)  {
-        while (getline(cin, line))
-            cout << line << endl ;
+    else if (argc == 1)  {
+        while (read(fileno(stdin), input_line, MAX_LEN) > 0){
+            size_t es = write(fileno(stdout), input_line, strlen(input_line)+1);
+            if(es <= 0){
+                printf(stderr,"no se puede escribir en la salida\n ");
+                exit(EXIT_FAILURE);
+            }
+            memset(input_line,0,MAX_LEN);
+        }      
     }
     else {
-        string namefile = argv[1];
-        ifstream MyReadFile(namefile);
-        while (getline(MyReadFile, line))
-            cout << line<< endl;
-        
+        printf(stderr, "Uso del programa: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
     return 0;
 }
